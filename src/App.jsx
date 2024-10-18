5fb99d3e94443aabaf24c53d4940131b4479433c
DussanFreire <dussanfreire@gmail.com>
2024-10-17 21:04:48 -0400

    bug:diployement problem

--- src/App.jsx
+++ src/App.jsx
@@ -10,14 +10,14 @@ import CountryList from './components/CountryList';
 import City from './components/City';
 import Form from './components/Form';
 import SpinnerFullPage from './components/SpinnerFullPage';
-import { GoogleOAuthProvider } from '@react-oauth/google';
 
-const Homepage = lazy(() => import('./pages/Homepage'));
-const Product = lazy(() => import('./pages/Product'));
-const Pricing = lazy(() => import('./pages/Pricing'));
-const Login = lazy(() => import('./pages/Login'));
-const AppLayout = lazy(() => import('./pages/AppLayout'));
-const PageNotFound = lazy(() => import('./pages/PageNotFound'));
+import Product from './pages/Product';
+import Pricing from './pages/Pricing';
+import Homepage from './pages/Homepage';
+import Login from './pages/Login';
+import AppLayout from './pages/AppLayout';
+import PageNotFound from './pages/PageNotFound';
+import { GoogleOAuthProvider } from '@react-oauth/google';
 
 function App() {
   const clientId = import.meta.env.VITE_APP_CLIENT_ID;
